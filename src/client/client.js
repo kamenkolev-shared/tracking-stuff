892c0177e9c5166eb82a6bed14168ee194677bff
Kamen <kamen.kolev@scalefocus.com>
2022-02-27 16:25:48 +0200

    no work on kill

---
+++ src/client/client.js
@@ -0,0 +1,26 @@
+export const wsURL = "ws://localhost:5000"
+export const beaconURL = "http://localhost:5001"
+
+const ws = new WebSocket(wsURL)
+ws.onopen = () => {
+  ws.send("HELLO FROM CLIENT")
+}
+
+document.addEventListener("visibilitychange", function () {
+  // ! does not trigger initially
+  if (document.visibilityState === "visible") {
+    fetch(beaconURL, {
+      body: "FROM FETCH VISIBLE",
+      method: "POST",
+    })
+
+    navigator.sendBeacon(beaconURL, "FROM BEACON VISIBLE")
+  } else {
+    navigator.sendBeacon(beaconURL, "FROM BEACON HIDDEN")
+
+    fetch(beaconURL, {
+      body: "FROM FETCH HIDDEN",
+      method: "POST",
+    })
+  }
+})
---
+++ src/server/server.ts
@@ -0,0 +1,27 @@
+import { serve } from "https://deno.land/std/http/mod.ts"
+
+function WSHandler(req: Request) {
+  if (req.headers.get("upgrade") != "websocket") {
+    return new Response(null, { status: 501 })
+  }
+  const { socket, response } = Deno.upgradeWebSocket(req)
+  socket.addEventListener("message", e => {
+    console.log(e.data)
+  })
+  return response
+}
+
+serve(WSHandler, { port: 5000 })
+
+// beacon handler
+
+async function BeaconHandler(req: Request) {
+  const text = await req.text()
+  console.log({ text })
+
+  return new Response(null, { status: 200, headers:{
+    'Access-Control-Allow-Origin': '*'
+  } })
+}
+
+serve(BeaconHandler, { port: 5001 })
