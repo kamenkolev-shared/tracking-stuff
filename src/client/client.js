c4734cf4a9b1e9007f83948767d8831ac0735f0e
Kamen <kamen.kolev@scalefocus.com>
2022-02-27 18:31:54 +0200

    stuff

--- src/client/client.js
+++ src/client/client.js
@@ -1,30 +1,28 @@
 export const wsURL = "ws://localhost:5000"
 export const beaconURL = "http://localhost:5001"
 
+// TODO pingpong
 const ws = new WebSocket(wsURL)
 ws.onopen = () => {
   setInterval(() => {
     ws.send(new Date().getTime().toString())
   }, 200)
 }
+
+// ? for when tab is minimized / maximized / opened / gracefully closed
 // VS Page visibility API
 // Tabs which are playing audio are considered foreground and aren't throttled.
 // Tabs running code that's using real-time network connections (WebSockets and WebRTC) go unthrottled in order to avoid closing these connections timing out and getting unexpectedly closed.
 document.addEventListener("visibilitychange", function () {
   // ! does not trigger initially
   if (document.visibilityState === "visible") {
-    fetch(beaconURL, {
-      body: "FROM FETCH VISIBLE",
-      method: "POST",
-    })
-
-    navigator.sendBeacon(beaconURL, "FROM BEACON VISIBLE")
+    navigator.sendBeacon(beaconURL, "FROM visibilityState VISIBLE")
   } else {
-    navigator.sendBeacon(beaconURL, "FROM BEACON HIDDEN")
-
-    fetch(beaconURL, {
-      body: "FROM FETCH HIDDEN",
-      method: "POST",
-    })
+    navigator.sendBeacon(beaconURL, "FROM visibilityState HIDDEN")
   }
+})
+
+// ? For when app is not focused
+window.addEventListener('blur', e => {
+  navigator.sendBeacon(beaconURL, "Window blurred")
 })
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,17 +1,32 @@
 import { serve } from "https://deno.land/std/http/mod.ts"
 
+
+
 function WSHandler(req: Request) {
   if (req.headers.get("upgrade") != "websocket") {
     return new Response(null, { status: 501 })
   }
   const { socket, response } = Deno.upgradeWebSocket(req)
-  socket.addEventListener("message", e => {
-    console.log(e.data)
-  })
 
-  socket.onclose = (_e) => {
+  // socket.addEventListener()
+  socket.onclose = _e => {
     console.log("socket closed")
   }
+
+  socket.onerror = err => {
+    console.log("error", err)
+  }
+
+  socket.onmessage = e => {
+    console.log("socket message", e.data)
+  }
+
+  socket.onopen = (_e) => {
+    console.log("socket open")
+  }
+
+  socket.send('')
+  socket.onclose = console.log
   return response
 }
 
@@ -23,9 +38,12 @@ async function BeaconHandler(req: Request) {
   const text = await req.text()
   console.log({ text })
 
-  return new Response(null, { status: 200, headers:{
-    'Access-Control-Allow-Origin': '*'
-  } })
+  return new Response(null, {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  })
 }
 
 serve(BeaconHandler, { port: 5001 })
