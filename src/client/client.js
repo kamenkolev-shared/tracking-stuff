fb563b45d57124308d9b922c3fda33639ead4645
Kamen <kamen.kolev@scalefocus.com>
2022-02-27 20:43:43 +0200

    x

--- src/client/client.js
+++ src/client/client.js
@@ -1,13 +1,42 @@
 export const wsURL = "ws://localhost:5000"
 export const beaconURL = "http://localhost:5001"
+export const authURL = "http://localhost:5002"
+
+const messages = {
+  hidden: "page hidden",
+  visible: "page visible",
+  blurred: "window blurred",
+  focused: "window focused",
+
+  unload: "page unloading", // ? useless?
+}
+
+// @ts-ignore
+// await fetch(authURL).then(console.log, console.warn)
 
 // TODO pingpong
-const ws = new WebSocket(wsURL)
-ws.onopen = () => {
-  setInterval(() => {
-    ws.send(new Date().getTime().toString())
-  }, 200)
+const openWS = () => {
+  const ws = new WebSocket(wsURL)
+  let interval = null
+  ws.onopen = () => {
+    interval = setInterval(() => {
+      ws.send("ping")
+    }, 1000)
+  }
+
+  const handleClose = () => {
+    console.log("handleClose")
+    if (interval) {
+      clearInterval()
+    }
+    openWS()
+  }
+
+  ws.onclose = handleClose
+  ws.onerror = console.warn
 }
+
+openWS()
 
 // ? for when tab is minimized / maximized / opened / gracefully closed
 // VS Page visibility API
@@ -16,13 +45,21 @@ ws.onopen = () => {
 document.addEventListener("visibilitychange", function () {
   // ! does not trigger initially
   if (document.visibilityState === "visible") {
-    navigator.sendBeacon(beaconURL, "FROM visibilityState VISIBLE")
+    navigator.sendBeacon(beaconURL, messages.visible)
   } else {
-    navigator.sendBeacon(beaconURL, "FROM visibilityState HIDDEN")
+    navigator.sendBeacon(beaconURL, messages.hidden)
   }
 })
 
 // ? For when app is not focused
-window.addEventListener('blur', e => {
-  navigator.sendBeacon(beaconURL, "Window blurred")
+window.addEventListener("blur", _e => {
+  navigator.sendBeacon(beaconURL, messages.blurred)
+})
+
+window.addEventListener("focus", _e => {
+  navigator.sendBeacon(beaconURL, messages.focused)
+})
+
+window.addEventListener("unload", _e => {
+  navigator.sendBeacon(beaconURL, messages.unload)
 })
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,6 +1,5 @@
 import { serve } from "https://deno.land/std/http/mod.ts"
-
-
+// should userID really be sent from the client? can it be trusted?
 
 function WSHandler(req: Request) {
   if (req.headers.get("upgrade") != "websocket") {
@@ -8,25 +7,33 @@ function WSHandler(req: Request) {
   }
   const { socket, response } = Deno.upgradeWebSocket(req)
 
-  // socket.addEventListener()
   socket.onclose = _e => {
-    console.log("socket closed")
+    console.log("SOCKET CLOSED!")
   }
 
-  socket.onerror = err => {
-    console.log("error", err)
+  socket.onerror = _e => {
+    console.log("SOCKET ERRORED")
   }
+
+  let timerHandle = setTimeout(() => {
+    socket.close()
+  }, 3000)
 
   socket.onmessage = e => {
-    console.log("socket message", e.data)
+    console.log("SOCKET MESSAGE")
+    if (e.data === "ping") {
+      clearTimeout(timerHandle)
+      // close socket after assumed 30s network issue
+      timerHandle = setTimeout(() => {
+        socket.close()
+      }, 30000)
+    }
   }
 
-  socket.onopen = (_e) => {
-    console.log("socket open")
+  socket.onopen = _e => {
+    console.log("SOCKET OPEN")
   }
 
-  socket.send('')
-  socket.onclose = console.log
   return response
 }
 
@@ -47,3 +54,19 @@ async function BeaconHandler(req: Request) {
 }
 
 serve(BeaconHandler, { port: 5001 })
+
+function AuthHandler(_req: Request) {
+  const res = new Response(null, {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Set-Cookie": `id=a3fWa; Max-Age=2592000; HttpOnly`,
+    },
+  })
+
+  return res
+}
+
+serve(AuthHandler, { port: 5002 })
+
+// TODO websocket user session
