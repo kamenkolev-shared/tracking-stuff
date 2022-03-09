e46b6771b1b2845c3744484c5568a91fecaece45
Kamen <kamen.kolev@scalefocus.com>
2022-03-09 14:35:41 +0200

    storage of failed beacons

--- src/client/client.js
+++ src/client/client.js
@@ -1,5 +1,31 @@
 import { baseURL, userID } from "./shared.js"
 
+/**
+ *
+ * @param {BodyInit} data
+ */
+const sendEvent = data => {
+  const queued = navigator.sendBeacon(eventLogUrl, data)
+  if (!queued) {
+    /**
+     * @type {Array}
+     */
+    const failedBeacons = JSON.parse(
+      localStorage.getItem("failed-beacons") ?? "[]",
+    )
+    localStorage.setItem(
+      "failed-beacons",
+      JSON.stringify(
+        failedBeacons.concat({
+          time: Date.now(),
+          url: eventLogUrl,
+          data,
+        }),
+      ),
+    )
+  }
+}
+
 const wsURL = `wss://${baseURL}/ws?userID=${userID}`
 const eventLogUrl = `https://${baseURL}/log?userID=${userID}`
 const events = {
@@ -39,8 +65,7 @@ openWS()
 // ? for when tab is minimized / maximized / opened / gracefully closed
 
 document.addEventListener("visibilitychange", () => {
-  navigator.sendBeacon(
-    eventLogUrl,
+  sendEvent(
     document.visibilityState === "visible"
       ? events.pageVisible
       : events.pageHide,
@@ -48,20 +73,21 @@ document.addEventListener("visibilitychange", () => {
 })
 
 // ? For when app is not focused
-window.addEventListener("blur", _e => {
-  navigator.sendBeacon(eventLogUrl, events.windowBlur)
+window.addEventListener("blur", () => {
+  sendEvent(events.windowBlur)
 })
 
-window.addEventListener("focus", _e => {
-  navigator.sendBeacon(eventLogUrl, events.windowFocus)
+window.addEventListener("focus", () => {
+  sendEvent(events.windowFocus)
 })
 
-window.addEventListener("unload", _e => {
-  navigator.sendBeacon(eventLogUrl, events.windowUnload)
+window.addEventListener("unload", () => {
+  sendEvent(events.windowUnload)
 })
 
 // could it be visible, but not focused?
-navigator.sendBeacon(
-  eventLogUrl,
-  document.visibilityState === "visible" ? events.initVisible : events.initHidden,
+sendEvent(
+  document.visibilityState === "visible"
+    ? events.initVisible
+    : events.initHidden,
 )
